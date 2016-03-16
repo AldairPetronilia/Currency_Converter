3b57394dcead6588d7a9ec99074ee2872e1f24b2
aldairpetronilia <aldairpetronilia@gmail.com>
2016-03-16 20:49:02 +0000

    Functionality has been added to allow the program to read all of teh currency codes in a files and store them in a map while creating a currency object for each currency.

---
+++ app/src/main/java/com/example/aldairpetronilia/currencyconverter/Currency.java
@@ -0,0 +1,68 @@
+package com.example.aldairpetronilia.currencyconverter;
+
+/**
+ * Created by aldairpetronilia on 16/03/2016.
+ */
+
+public class Currency {
+
+    private String currencyCode;
+    private String currencyName;
+    private double rate;
+    private String currencySymbol;
+
+    public Currency(String currencyCode, String currencyName) {
+        this.currencyCode = currencyCode;
+        this.currencyName = currencyName;
+    }
+
+    public Currency( String currencyCode, String currencyName, double rate) {
+        this.currencyName = currencyName;
+        this.currencyCode = currencyCode;
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "currencyCode='" + currencyCode + '\'' +
+                ", currencyName='" + currencyName + '\'' +
+                ", rate=" + rate +
+                ", currencySymbol='" + currencySymbol + '\'' +
+                '}';
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
+
+    public void setCurrencySymbol(String currencySymbol) {
+        this.currencySymbol = currencySymbol;
+    }
+
+
+}
--- app/src/main/java/com/example/aldairpetronilia/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/aldairpetronilia/currencyconverter/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.aldairpetronilia.currencyconverter;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Map<String, Currency> allCurrency = new HashMap<String, Currency>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +33,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createCurrency();
+    }
+
+    private void createCurrency()  {
+
+        AssetManager assetManager = getAssets();
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
+        String nextCurrency;
+
+        try {
+            inputStream = assetManager.open("currencyCodes.txt");
+            bufferedReader= new BufferedReader(new InputStreamReader(inputStream));
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            Log.i("Scanner Info", "Error Occurred");
+
+        } finally {
+            if (inputStream == null){
+                System.out.println("Error Occurred");
+            } else {
+                System.out.println("Currency Code File Access Success");
+            }
+        }
+
+        try {
+            while ((nextCurrency = bufferedReader.readLine()) != null){
+                allCurrency.put(nextCurrency.substring(0,3), new Currency(nextCurrency.substring(0,3),nextCurrency.substring(4)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
